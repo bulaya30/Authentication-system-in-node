d25a8c32335edafb951e50f9a1d9d857253f24c7
bulaya30 <66732376+bulaya30@users.noreply.github.com>
2025-12-09 23:58:39 +0300

    Refactor database connection to use ES6 modules

--- app/database/db.js
+++ app/database/db.js
@@ -1,12 +1,27 @@
-const driver = require('mongoose')
-const connect = async ()=>{
-    try {
-        driver.set('strictQuery', false)
-        const con = await driver.connect(process.env.dbUrl)
-        console.log(`Connection established... ${con.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
-}
+//const driver = require('mongoose')
+//const connect = async ()=>{
+  //  try {
+    //    driver.set('strictQuery', false)
+      //  const con = await driver.connect(process.env.dbUrl)
+        //console.log(`Connection established... ${con.connection.host}`);
+    //} catch (error) {
+      //  console.log(error);
+    //}
+//}
 
-module.exports = connect
+
+//module.exports = connect
+
+import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected ✅");
+  } catch (error) {
+    console.error("MongoDB error ❌", error);
+    process.exit(1);
+  }
+};
+
+export default connect;
